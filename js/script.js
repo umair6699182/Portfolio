e547aca328f1078f559922bdac3cc4b1665942fb
Umair Malik <121504980+umair6699182@users.noreply.github.com>
2023-05-27 05:58:35 -0700

    Inatial change

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ------------- typing animation --------------
+var typed = new Typed('.typing' , {
+    strings:["," , "Web Designer" , "Full Stack Developer", "YouTuber"],
+    typeSpeed:100,
+    BackSpeed:100,
+    loop:true
+})
